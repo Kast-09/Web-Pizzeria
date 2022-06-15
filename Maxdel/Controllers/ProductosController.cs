62398c6c870b4228d8b8d24fb74c6837250a1783
Kast-09 <kevinaron.234@gmail.com>
2022-06-15 18:55:38 -0500

    Agregar Tamaño/Precio OK

--- Maxdel/Controllers/ProductosController.cs
+++ Maxdel/Controllers/ProductosController.cs
@@ -28,11 +28,45 @@ namespace Maxdel.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult AgregarProducto(Productos productos)
         {
             _dbEntities.Productos.Add(productos);
+            _dbEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditarProducto(int Id)
+        {
+            var Producto = _dbEntities.Productos.First(o => o.Id == Id);
+            return View(Producto);
+        }
+        [HttpPost]
+        public IActionResult EditarProducto(int Id, Productos productos)
+        {
+            var producto = _dbEntities.Productos.First(o => o.Id == Id);
+            producto.Nombre = productos.Nombre;
+            producto.Descripcion = productos.Descripcion;
+            producto.UrlImagen = productos.UrlImagen;
+            _dbEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AgregarTamañoPrecio(int Id)
+        {
+            var Producto = _dbEntities.Productos.First(o => o.Id == Id);
+            return View(Producto);
+        }
+        [HttpPost]
+        public IActionResult AgregarTamañoPrecio(int Id, TamañoPrecio tamañoPrecio)
+        {
+            TamañoPrecio tamañoPrecio1 = new TamañoPrecio();
+            tamañoPrecio1.IdProducto = Id;
+            tamañoPrecio1.TamañoProducto = tamañoPrecio.TamañoProducto;
+            tamañoPrecio1.Precio = tamañoPrecio.Precio;
+            _dbEntities.tamañoPrecios.Add(tamañoPrecio1);
             _dbEntities.SaveChanges();
             return RedirectToAction("Index");
         }
