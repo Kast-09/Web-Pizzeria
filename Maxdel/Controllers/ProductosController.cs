5e30b1225f04eacf321878ab8c989b8977494438
Kast-09 <kevinaron.234@gmail.com>
2022-06-15 18:01:37 -0500

    Agregar Producto OK

--- Maxdel/Controllers/ProductosController.cs
+++ Maxdel/Controllers/ProductosController.cs
@@ -1,9 +1,12 @@
 ﻿using Maxdel.DB;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Maxdel.Models;
 
 namespace Maxdel.Controllers
 {
+    [Authorize]
     public class ProductosController : Controller
     {
         private readonly DbEntities _dbEntities;
@@ -18,6 +21,20 @@ namespace Maxdel.Controllers
             var ListaProductos = _dbEntities.Productos.ToList();
             ViewBag.Tamaños = _dbEntities.tamañoPrecios.ToList();
             return View(ListaProductos);
+        }
+
+        [HttpGet]
+        public IActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AgregarProducto(Productos productos)
+        {
+            _dbEntities.Productos.Add(productos);
+            _dbEntities.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
