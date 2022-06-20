b3408a2122f168bc9debf7b1a8f3f9dc76405edb
Kast-09 <kevinaron.234@gmail.com>
2022-06-20 03:34:52 -0500

    Reporte de ventas OK

---
+++ Maxdel/Controllers/VentasController.cs
@@ -0,0 +1,34 @@
+﻿using Maxdel.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maxdel.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly DbEntities _dbEntities;
+
+        public VentasController(DbEntities dbEntities)
+        {
+            _dbEntities = dbEntities;
+        }
+
+        public IActionResult Index()
+        {
+            var boletas = _dbEntities.boletas
+                                .Include("Pedidos")
+                                .Where(o => o.Pedidos.Any(x => x.Estado == 5)).ToList();
+            
+            decimal subtotal = 0;
+
+            foreach (var item in boletas)
+            {
+                subtotal += item.MontoTotal;
+            }
+
+            ViewBag.Subtotal = subtotal;
+
+            return View(boletas);
+        }
+    }
+}
